bf25edd37d2d78621a0840c27a13851f6b563374
SNadim <93469458+SNadim@users.noreply.github.com>
2022-09-01 01:18:58 +0600

    first commit

---
+++ StripeWebApp/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Stripe;
+using Stripe.Checkout;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StripeWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateCheckoutSession(string amount)
+        {
+            var option = new Stripe.Checkout.SessionCreateOptions
+            {
+                LineItems = new List<SessionLineItemOptions>
+                {
+                    new SessionLineItemOptions
+                    {
+                        PriceData = new SessionLineItemPriceDataOptions
+                        {
+                            UnitAmount = Convert.ToInt32(amount)*100,
+                            Currency = "inr",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                                Name = "T-Shirt",
+                            },
+                        },
+                        Quantity = 1,
+                    },
+                },
+
+                Mode = "payment",
+                SuccessUrl = "https://localhost:44354/Home/About",
+                CancelUrl = "https://localhost:44354/Home/Contact",
+            };
+            var service = new Stripe.Checkout.SessionService();
+            Stripe.Checkout.Session session = service.Create(option);
+
+            Response.Headers.Add("Location", session.Url);
+            return new HttpStatusCodeResult(303);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ StripeWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StripeWebApp.MvcApplication" Language="C#" %>
